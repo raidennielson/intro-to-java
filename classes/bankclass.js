4f4d16706d3a074f538ec670af00fca8118974b5
Web Development <raidennielson968@gmail.com>
2024-09-17 12:14:22 -0600

    Update bankclass.js

--- classes/bankclass.js
+++ classes/bankclass.js
@@ -1,7 +1,19 @@
+let withdrawal = 0
+let deposit = 0
 class BankAccount {
     #accountNumber
     #balance
-    deposit(amount);
-    withdraw(amount)
-
+    deposit(){
+(this.#balance + deposit) = this.#balance;
+return this.#balance
+}
+    withdraw()
+{
+(this.#balance - withdrawal) = this.#balance;
+if(this.#balance <= 0){
+   console.log("insufficient funds") 
+}else{
+    return this.#balance
+   }
+}
 }
